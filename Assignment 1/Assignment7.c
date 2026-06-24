0d7a1c8b43ea1447624ed9432a3be5683e3670e0
ANSHUMAN <anshuman01022006@gmail.com>
2026-06-24 14:05:32 +0530

    Create Assignment7.c

---
+++ Assignment 1/Assignment7.c
@@ -0,0 +1,8 @@
+// WAP in C to calculate average of three numbers.
+#include <stdio.h>
+int main() {
+    float a = 10, b = 20, c = 30;
+    float avg = (a + b + c) / 3;
+    printf("Average = %.2f", avg);
+    return 0;
+}
