46c95297e0d21906fb5788659274fa57bd568ccb
ANSHUMAN <anshuman01022006@gmail.com>
2026-06-24 14:04:20 +0530

    Create Assignment5.c

---
+++ Assignment 1/Assignment5.c
@@ -0,0 +1,7 @@
+// WAP in C to print character of given ASCII value.
+#include <stdio.h>
+int main() {
+    int ascii = 65;
+    printf("Character for ASCII %d is: %c", ascii, ascii);
+    return 0;
+}
