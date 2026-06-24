a2560704df60b8938efd14f39e97b32d3f53aea4
ANSHUMAN <anshuman01022006@gmail.com>
2026-06-24 14:01:13 +0530

    Create Assignment4.c

---
+++ Assignment 1/Assignment4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char name[50];
+    int roll;
+    printf("Enter student name: ");
+    scanf("%s", name);
+    printf("Enter roll number: ");
+    scanf("%d", &roll);
+    
+    printf("\nStudent Name: %s\n", name);
+    printf("Roll Number: %d", roll);
+    return 0;
+}
